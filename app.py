6e2ec92b5ffb8ff63daefbc33ea846efc650746a
iomiras <miras.shaltayev@gmail.ru>
2024-11-14 14:39:13 +0500

    final

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 load_dotenv()
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
